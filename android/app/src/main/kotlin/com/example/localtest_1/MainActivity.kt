f3f7667f8053f62c825da14dfc95153eb469d062
gpark <pcm04103@gmail.com>
2023-07-20 14:07:57 +0900

    2023.07.20 플러터 로그인 페이지 테스트 업로드

---
+++ android/app/src/main/kotlin/com/example/localtest_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.localtest_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widget/login_email.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class login_email extends StatelessWidget {
+  final String Title;
+  final Color TitleColor;
+  //login background values
+
+  const login_email({super.key, 
+    required this.Title,
+    required this.TitleColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+    ,)
+  }
+}
